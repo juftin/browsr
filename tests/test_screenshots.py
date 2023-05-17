3a63a560a0cc55a4c6ed2c400fe9e8a5c186686d
Justin Flannery <juftin@juftin.com>
2023-05-17 16:55:53 -0600

    🧪 screenshot tests

---
+++ tests/test_screenshots.py
@@ -0,0 +1,29 @@
+"""
+Screenshot Testing Using Cassettes!
+"""
+
+from browsr.universal_directory_tree import GitHubPath
+from tests.conftest import cassette
+from tests.helpers import Screenshotter
+
+
+@cassette
+def test_github_screenshot(github_release_path: GitHubPath) -> None:
+    """
+    Snapshot a release of this repo
+    """
+    file_path = str(github_release_path)
+    screenshotter = Screenshotter(file_path=file_path)
+    screenshot, screenshot_path = screenshotter.take_screenshot()
+    assert screenshot_path.read_text() == screenshot
+
+
+@cassette
+def test_github_screenshot_license(github_release_path: GitHubPath) -> None:
+    """
+    Snapshot the LICENSE file
+    """
+    file_path = str(github_release_path / "LICENSE")
+    screenshotter = Screenshotter(file_path=file_path)
+    screenshot, screenshot_path = screenshotter.take_screenshot()
+    assert screenshot_path.read_text() == screenshot
