d26cdcd338c85ed413f307d03bef2728cddf6b87
juftin <juftin@juftin.com>
2023-12-21 19:08:06 -0700

    🧪 pytest-textual-snapshot

--- tests/test_screenshots.py
+++ tests/test_screenshots.py
@@ -2,29 +2,55 @@
 Screenshot Testing Using Cassettes!
 """
 
+import pathlib
+from textwrap import dedent
+from typing import Callable
+
+import pytest
 from textual_universal_directorytree import GitHubPath
 
 from tests.conftest import cassette
-from tests.helpers import Screenshotter
+
+
+@pytest.fixture
+def app_file() -> str:
+    file_content = """
+    from browsr import Browsr
+    from browsr._base import TextualAppContext
+
+    file_path = "{file_path}"
+    context = TextualAppContext(file_path=file_path)
+    app = Browsr(config_object=context)
+    """
+    return dedent(file_content).strip()
 
 
 @cassette
-def test_github_screenshot(github_release_path: GitHubPath) -> None:
+def test_github_screenshot(
+    snap_compare: Callable[..., bool],
+    tmp_path: pathlib.Path,
+    app_file: str,
+    github_release_path: GitHubPath,
+) -> None:
     """
     Snapshot a release of this repo
     """
-    file_path = str(github_release_path)
-    screenshotter = Screenshotter(file_path=file_path)
-    screenshot, screenshot_path = screenshotter.take_screenshot()
-    assert screenshot_path.read_text() == screenshot
+    app_path = tmp_path / "app.py"
+    app_path.write_text(app_file.format(file_path=str(github_release_path)))
+    assert snap_compare(app_path=app_path, terminal_size=(160, 48))
 
 
 @cassette
-def test_github_screenshot_license(github_release_path: GitHubPath) -> None:
+def test_github_screenshot_license(
+    snap_compare: Callable[..., bool],
+    tmp_path: pathlib.Path,
+    app_file: str,
+    github_release_path: GitHubPath,
+) -> None:
     """
     Snapshot the LICENSE file
     """
     file_path = str(github_release_path / "LICENSE")
-    screenshotter = Screenshotter(file_path=file_path)
-    screenshot, screenshot_path = screenshotter.take_screenshot()
-    assert screenshot_path.read_text() == screenshot
+    app_path = tmp_path / "app.py"
+    app_path.write_text(app_file.format(file_path=file_path))
+    assert snap_compare(app_path=app_path, terminal_size=(160, 48))
