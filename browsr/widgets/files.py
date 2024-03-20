bbc1d7d2a68ab5252dade463b9f145af9829d08d
Justin Flannery <juftin@juftin.com>
2024-03-20 13:57:18 -0600

    ✨ FileInfo

--- browsr/widgets/files.py
+++ browsr/widgets/files.py
@@ -7,6 +7,12 @@ from rich.text import Text
 from textual.message import Message
 from textual.reactive import reactive
 from textual.widget import Widget
+from textual_universal_directorytree import (
+    GitHubTextualPath,
+    S3TextualPath,
+    SFTPTextualPath,
+    is_remote_path,
+)
 
 from browsr.utils import FileInfo
 
@@ -56,23 +62,77 @@ class CurrentFileInfoBar(Widget):
         """
         Render the Current File Info Bar
         """
-        if self.file_info is None or not self.file_info.is_file:
-            return Text("")
-        status_string = "🗄️️️  " + self._convert_size(self.file_info.size)
+        status_string = self.render_file_protocol()
+        if self.file_info is None:
+            return Text(status_string)
+        elif self.file_info.is_file:
+            file_options = self.render_file_options()
+            status_string += file_options
         if (
             self.file_info.last_modified is not None
             and self.file_info.last_modified.timestamp() != 0
         ):
             modify_time = self.file_info.last_modified.strftime("%b %d, %Y %I:%M %p")
-            status_string += "  📅  " + modify_time
-        parent_name = self.file_info.file.parent.name
-        if not parent_name:
-            parent_name = str(self.file_info.file.parent)
-            parent_name = parent_name.lstrip(f"{self.file_info.file.protocol}://")
-            parent_name = parent_name.rstrip("/")
-        status_string += "  💾  " + self.file_info.file.name + "  📂  " + parent_name
+            status_string += f"  📅  {modify_time}"
+        directory_options = self.render_directory_options()
+        status_string += directory_options
+        return Text(status_string.strip(), style="dim")
+
+    def render_file_options(self) -> str:
+        """
+        Render the file options
+        """
+        status_string = ""
+        if not self.file_info:
+            return status_string
+        if self.file_info.is_file:
+            file_size = self._convert_size(self.file_info.size)
+            status_string += f"  🗄️️  {file_size}"
         if self.file_info.owner not in ["", None]:
-            status_string += "  👤  " + self.file_info.owner
+            status_string += f"  👤  {self.file_info.owner}"
         if self.file_info.group.strip() not in ["", None]:
-            status_string += "  🏠  " + self.file_info.group
-        return Text(status_string, style="dim")
+            status_string += f"  🏠  {self.file_info.group}"
+        return status_string
+
+    def render_directory_options(self) -> str:
+        """
+        Render the directory options
+        """
+        status_string = ""
+        if not self.file_info:
+            return status_string
+        if self.file_info.is_file:
+            directory_name = self.file_info.file.parent.name
+            if not directory_name or (
+                self.file_info.file.protocol
+                and f"{self.file_info.file.protocol}://" in directory_name
+            ):
+                directory_name = str(self.file_info.file.parent)
+                directory_name = directory_name.lstrip(
+                    f"{self.file_info.file.protocol}://"
+                )
+                directory_name = directory_name.rstrip("/")
+            status_string += f"  📂  {directory_name}"
+            status_string += f"  💾  {self.file_info.file.name}"
+        else:
+            status_string += f"  📂  {self.file_info.file.name}"
+        return status_string
+
+    def render_file_protocol(self) -> str:
+        """
+        Render the file protocol
+        """
+        status_string = ""
+        if not self.file_info:
+            return status_string
+        if is_remote_path(self.file_info.file):
+            if isinstance(self.file_info.file, GitHubTextualPath):
+                protocol = "GitHub"
+            elif isinstance(self.file_info.file, S3TextualPath):
+                protocol = "S3"
+            elif isinstance(self.file_info.file, SFTPTextualPath):
+                protocol = "SFTP"
+            else:
+                protocol = self.file_info.file.protocol
+            status_string += f"🗂️  {protocol}"
+        return status_string
